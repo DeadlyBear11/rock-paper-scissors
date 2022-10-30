490b4bf6f4355e5525da1bafd4c20d6720f70288
guijeco <86424543+DeadlyBear11@users.noreply.github.com>
2022-10-30 13:26:47 -0600

    Created function to play one round

--- scripts.js
+++ scripts.js
@@ -1,25 +1,61 @@
-// Computer randomly returns rock, paper or scissors.
+// Function for the computer to randomly return rock, paper or scissors.
 function getComputerChoice() {
-    let choice = Math.ceil(Math.random() * (4 - 1));
+    let computerSelection = Math.ceil(Math.random() * (4 - 1));
 
-    switch (choice) {
+    switch (computerSelection) {
         case 1:
-            choice = "rock";
+            computerSelection = "rock";
             break;
         case 2:
-            choice = "paper";
+            computerSelection = "paper";
             break;
         case 3:
-            choice = "scissors";
+            computerSelection = "scissors";
     }
 
-    return choice;
+    return computerSelection;
 }
 
-// Play a single round.
+// Function to play a single round.
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == "rock") {
+        switch (computerSelection) {
+            case "scissors":
+                return "You win! Rock beats scissors.";
+            case "paper":
+                return "You lose! Paper beats rock.";
+            case "rock":
+                return "It's a tie! Both selected rock.";
+        }
+    } else if (playerSelection == "paper") {
+        switch (computerSelection) {
+            case "scissors":
+                return "You lose! Scissors beats paper.";
+            case "paper":
+                return "It's a tie! Both selected paper.";
+            case "rock":
+                return "You win! Paper beats rock.";
+        }
+    } else if (playerSelection == "scissors") {
+        switch (computerSelection) {
+            case "scissors":
+                return "It's a tie! Both selected scissors.";
+            case "paper":
+                return "You win! Scissors beats paper.";
+            case "rock":
+                return "You lose! Rock beats scissors.";
+        }
+    } else {
+        return "I don't understand your selection.";
+    }
+}
 // Ask for input and save it in a variable.
-// Compare the player selection and the computer selection.
-// Display winner of the round.
+let playerSelection = (prompt("Type 'rock', 'paper' or 'scissors':")).toLowerCase();
+
+// Compare the player selection and the computer selection
+// and display winner of the round.
+console.log(playRound(playerSelection,getComputerChoice()));
+
 // Play five consecutive rounds and keep the score.
 // Display the score after each round.
 // Show the final score and the win or loss of the player.
